7c6d73e92f07e8abd9a742b9eb00dc757cb2e48c
ronaov <giangthongthai2006@gmail.com>
2025-01-11 23:37:52 +0700

    update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function tao() {
   var value = link.value
   if (value.includes('drive.google.com')) {
     var a = "s" + (Math.floor(Math.random() * 999999999) + 999999999)
-    var b = ten.value
+    var b = encodeURIComponent(ten.value)
     var k = value.slice(32, value.indexOf("/", 32))
     const newURL = `https://ronaov.github.io/DisposableWeb/link/?a=${encodeURIComponent(k)}&id=${a}&b=${b}`
     navigator.clipboard.writeText(newURL)
