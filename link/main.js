b1dd2d0d0e9ebadbd5495072afecec17be333e6a
ronaov <giangthongthai2006@gmail.com>
2025-01-12 15:51:59 +0700

    update

--- link/main.js
+++ link/main.js
@@ -1,37 +1,57 @@
-const url = new URLSearchParams(window.location.search)
-const h4 = document.getElementById('h4')
-const down = document.getElementById('down')
-const filename = url.get('b')
+const bg = document.getElementById('container')
+const u = new URLSearchParams(window.location.search)
+const anh = document.getElementById('anh')
+const ten = document.getElementById('ten')
+const logo = document.getElementById('logo')
+const td = document.getElementById('td')
 
-var k = url.get('a')
-const id = url.get('id')
-url.delete('a')
-if (k != null) {
-  localStorage.setItem(id, k)
-} else {
-  k = localStorage.getItem(id)
+const link = u.get('a')
+const id = u.get('id')
+
+const link0 = "https://drive.google.com/uc?export=download&id=" + link
+
+if (link != "" && link) {
+  localStorage.setItem(id, link)
 }
 
-window.history.replaceState(null, '', window.location.pathname + "?" + url.toString())
+u.delete('a')
 
-const link = "https://drive.google.com/uc?export=download&id=" + k + "&usp=drive_web"
-if (k != null) {
-  if (filename != "") {
-    var name = filename
-  } else {
-    var name = "Link tải đã sẵn sàng"
-  }
-} else {
-  var name = "Xin hãy vượt link"
-  down.style.background = "red"
-  down.innerText = "Can't Download"
+const newlink = window.location.pathname + "?" + u.toString()
+history.replaceState(null, '', newlink)
+
+ten.innerText = u.get('c')
+if (u.get('b') != "") {
+  td.innerText = u.get('b')
 }
-h4.innerText = name
 
-function tai() {
-  if (k != null) {
-    window.location.href = link
-    down.style.background = "gray"
-    down.innerText = "Downloading..."
+
+const av = "https://imgur.com/" + u.get('d') + ".jpg"
+var bgid = "https://dl.ops.kgtw.garenanow.com/CHT/HeroTrainingLoadingNew_B36/" + u.get('e') + ".jpg"
+
+if (!localStorage.getItem(id)) {
+  bgid = Math.floor(Math.random()*3) + ".jpg"
+  td.innerHTML = "Thế này là không ổn rồi"
+  btn.style.background = "gray"
+  btn.innerHTML = "VƯỢT LINK ĐI MÁ"
+}
+
+var test = document.createElement('img')
+test.setAttribute('src', bgid)
+test.onload = function (){
+  bg.style.backgroundImage = `url('${bgid}')`
+  anh.src = bgid
+}
+
+var test2 = document.createElement('img')
+test2.setAttribute('src', av)
+test2.onload = function () {
+  logo.src = av
+}
+
+function taixuong() {
+  if (localStorage.getItem(id)) {
+    window.location.href = link0
+    btn.style.background = "gray"
+    btn.innerText = "DOWLOADING..."
   }
 }
--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const link = document.getElementById('link')
 const h5 = document.getElementById('h5')
 const make = document.getElementById('make')
 const ten = document.getElementById('ten')
+const csh = document.getElementById('csh')
+const av = document.getElementById('av')
+const id = document.getElementById('id')
+
 
 function check() {
   var value = link.value
@@ -14,14 +18,29 @@ function check() {
   }
 }
 
+csh.value = localStorage.getItem("csh") || "";
+av.value = localStorage.getItem("av") || "";
+
+function lcsh() {
+  localStorage.setItem("csh", csh.value)
+}
+
+function lk() {
+  if (av.value.includes('imgur.com')) {
+    localStorage.setItem("av", av.value)
+  }
+}
+
 function tao() {
   var value = link.value
   if (value.includes('drive.google.com')) {
     var a = "s" + (Math.floor(Math.random() * 999999999) + 999999999)
     var b = encodeURIComponent(ten.value)
     var k = value.slice(32, value.indexOf("/", 32))
-    const newURL = `https://ronaov.github.io/DisposableWeb/link/?a=${encodeURIComponent(k)}&id=${a}&b=${b}`
-    navigator.clipboard.writeText(newURL)
+    const newURL = `link/?a=${encodeURIComponent(k)}&id=${a}&b=${b}&c=${encodeURIComponent(csh.value)}&d=${av.value.slice(18)}&e=${id.value}`
+    //navigator.clipboard.writeText(newURL)
+    window.location.href = newURL
+    
     h5.innerText = "Đã tạo sao sao chép"
   }
 }
