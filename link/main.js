b2d598f7fc299901a12c8b29962c6c05144c1e40
ronaov <giangthongthai2006@gmail.com>
2025-01-13 11:39:22 +0700

    update

--- link/main.js
+++ link/main.js
@@ -7,6 +7,7 @@ const td = document.getElementById('td')
 
 var link = u.get('a')
 const id = u.get('id')
+const youtube = "https://youtube.com/@" + u.get('f')
 
 if (link != "" && link) {
   localStorage.setItem(id, link)
@@ -33,8 +34,8 @@ var bgid = "https://dl.ops.kgtw.garenanow.com/CHT/HeroTrainingLoadingNew_B36/" +
 if (!localStorage.getItem(id)) {
   bgid = Math.floor(Math.random()*3) + ".jpg"
   td.innerHTML = "Thế này là không ổn rồi"
-  btn.style.background = "gray"
-  btn.innerHTML = "VƯỢT LINK ĐI MÁ"
+  btn.style.background = "red"
+  btn.innerHTML = `<i class='bx bxl-youtube'></i> ${u.get('f').toUpperCase}`
 }
 
 var test = document.createElement('img')
@@ -55,5 +56,7 @@ function taixuong() {
     window.location.href = link0
     btn.style.background = "gray"
     btn.innerText = "DOWLOADING..."
+  } else {
+    window.location.href = youtube
   }
 }
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const ten = document.getElementById('ten')
 const csh = document.getElementById('csh')
 const av = document.getElementById('av')
 const id = document.getElementById('id')
-
+const kenh = document.getElementById('kenh')
 
 function check() {
   var value = link.value
@@ -18,8 +18,9 @@ function check() {
   }
 }
 
-csh.value = localStorage.getItem("csh") || "";
-av.value = localStorage.getItem("av") || "";
+csh.value = localStorage.getItem("csh") || ""
+av.value = localStorage.getItem("av") || ""
+kenh.value = localStorage.getItem("kenh") || ""
 
 function lcsh() {
   localStorage.setItem("csh", csh.value)
@@ -31,13 +32,17 @@ function lk() {
   }
 }
 
+function lkenh() {
+  localStorage.setItem("kenh", kenh.value)
+}
+
 function tao() {
   var value = link.value
   if (value.includes('drive.google.com')) {
     var a = "s" + (Math.floor(Math.random() * 999999999) + 999999999)
     var b = encodeURIComponent(ten.value)
     var k = value.slice(32, value.indexOf("/", 32))
-    const newURL = `https://ronaov.github.io/DisposableWeb/link/?a=${encodeURIComponent(k)}&id=${a}&b=${b}&c=${encodeURIComponent(csh.value)}&d=${av.value.slice(18)}&e=${id.value}`
+    const newURL = `https://ronaov.github.io/DisposableWeb/link/?a=${encodeURIComponent(k)}&id=${a}&b=${b}&c=${encodeURIComponent(csh.value)}&d=${av.value.slice(18)}&e=${id.value}&f=${kenh.value}`
     navigator.clipboard.writeText(newURL)
     
     h5.innerText = "Đã sao chép vào bàn phím"
